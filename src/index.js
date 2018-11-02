6d13e35cd7b1a979e0030434b8b1ca799d2a7d1c
Mario Vieira <mvieirasf@outlook.com>
2018-11-02 16:48:54 -0300

    (fix): added global styles

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
 import { store } from "./store";
 import App from "./containers/App";
 import GlobalColors from "./styles/settings/colors";
+import GlobalStyles from "./styles/settings/styles";
 
 ReactDOM.render(
   <Provider store={store}>
     <Fragment>
-      <GlobalColors />  
+      <GlobalColors />
+      <GlobalStyles />
       <App />
     </Fragment>
   </Provider>,
