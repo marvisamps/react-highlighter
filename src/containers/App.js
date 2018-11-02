2f2b2d805db073e5cf76c3e131055a608682911e
Mário Vieira <mvieirasf@outlook.com>
2018-11-02 03:11:04 -0300

    fix(state) use redux instead of local state

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,142 +1,84 @@
-import React, { Component } from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { bindActionCreators } from "redux";
+import { connect } from "react-redux";
 
-import * as Actions from '../actions';
+import * as Actions from "../actions";
 
-import TextArea from '../components/TextArea/';
-import Button from '../components/Button/';
-import InteractionText from '../components/InteractionText/';
+import TextArea from "../components/TextArea/";
+import Button from "../components/Button/";
+import InteractionText from "../components/InteractionText/";
 
-import '../App.css';
+import "../App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      textContent: 'Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Quem manda na minha terra sou euzis! Diuretics paradis num copo é motivis de denguis.',
-      highlightColor: 'yellowish',
-      yellowContent: [],
-      redContent: [],
-      greenContent: [],
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({ textContent: event.target.value })
-  }
-
-  handleClick = property => {
-    this.setState({ highlightColor: property })
-  }
-
-  handleFilter = property => {
-    if(property === 'yellow'){
-      this.setState({
-        yellowVisible: true,
-        redVisible: false,
-        greenVisible: false,
-      })
-    }
-
-    if(property === 'red'){
-      this.setState({
-        yellowVisible: false,
-        redVisible: true,
-        greenVisible: false,
-      })
-    }
-
-    if(property === 'green'){
-      this.setState({
-        yellowVisible: false,
-        redVisible: false,
-        greenVisible: true,
-      })
-    }
-  }
-
-  getSelectedText() {
-    let textSelection;
-    textSelection = (document.all) ?
-    document.getSelection.createRange().text :
-    document.getSelection();
-    return textSelection;
-  }
+  getSelectedText = () =>
+    document.all
+      ? document.getSelection.createRange().text
+      : document.getSelection();
 
   insertHighlight() {
-    let selection = this.getSelectedText();
-    let selectionContent = selection.toString();
+    const selection = this.getSelectedText();
+    const selectionContent = selection.toString();
 
-    let span = document.createElement('SPAN');
-
-    if(this.state.highlightColor === 'yellowish'){
-      span.className = 'yellowish';
-      this.setState(prevState => ({ yellowContent : [...prevState.yellowContent, selectionContent] }));
-    } 
-    
-    if(this.state.highlightColor === 'redish'){
-      span.className = 'redish';
-      this.setState(prevState => ({ redContent : [...prevState.redContent, selectionContent] }));
-    } 
-    
-    if(this.state.highlightColor === 'greenish'){
-      span.className = 'greenish';
-      this.setState(prevState => ({ greenContent : [...prevState.greenContent, selectionContent] }));
-    }
-
+    const span = document.createElement("SPAN");
+    span.className = this.props.visible.highlight;
     span.textContent = selectionContent;
 
-    let range = selection.getRangeAt(0);
+    this.props.setContent(this.props.visible.highlight, selectionContent);
+
+    const range = selection.getRangeAt(0);
     range.deleteContents();
     range.insertNode(span);
   }
 
   render() {
+    const {
+      changeVisible,
+      setHighlight,
+      setText,
+      visible: { visible, selections, text, highlight }
+    } = this.props;
+
+    const colors = ["yellow", "red", "green"];
+
     return (
       <div>
-        <TextArea value={this.state.textContent} onChange={this.handleChange} />
-        
-        <Button background="yellow" onClick={() => this.handleClick('yellowish')} label="Yellow" />
-        <Button background="red" onClick={() => this.handleClick('redish')} label="Red" />
-        <Button background="green" onClick={() => this.handleClick('greenish')} label="Green" />
+        <TextArea
+          value={text}
+          onChange={event => setText(event.target.value)}
+        />
 
-        <InteractionText 
-          className={this.state.highlightColor === 'yellowish' ? 'yellowishSelection' : this.state.highlightColor === 'redish' ? 'redishSelection' : 'greenishSelection'}
-          content={this.state.textContent}
+        {colors.map(item => (
+          <Button
+            background={item}
+            onClick={() => setHighlight(item)}
+            label={item}
+            key={item}
+          />
+        ))}
+
+        <InteractionText
+          className={`${highlight}Selection`}
+          content={text}
           onInteract={() => this.insertHighlight()}
         />
 
-        <Button background="yellow" onClick={() => this.handleFilter('yellow')} label="Show Yellow" />
-        <Button background="red" onClick={() => this.handleFilter('red')} label="Show Red" />
-        <Button background="green" onClick={() => this.handleFilter('green')} label="Show Green" />
+        {colors.map(item => (
+          <Button
+            background={item}
+            onClick={() => changeVisible(item)}
+            label={`Show ${item}`}
+            key={item}
+          />
+        ))}
 
-        {this.state.yellowVisible &&
+        {visible && (
           <ul>
-            {this.state.yellowContent.map(item =>
+            {selections[visible].map(item => (
               <li key={item}>{item}</li>
-            )}
+            ))}
           </ul>
-        }
-
-        {this.state.redVisible &&
-          <ul>
-            {this.state.redContent.map(item =>
-              <li key={item}>{item}</li>
-            )}
-          </ul>
-        }
-
-        {this.state.greenVisible &&
-          <ul>
-            {this.state.greenContent.map(item =>
-              <li key={item}>{item}</li>
-            )}
-          </ul>
-        }
+        )}
       </div>
     );
   }
@@ -144,10 +86,13 @@ class App extends Component {
 
 const mapStateToProps = state => ({
   ...state
-})
+});
 
 const mapDispatchToProps = dispatch => ({
-  actions: () => bindActionCreators(Actions, dispatch)
- })
+  ...bindActionCreators(Actions, dispatch)
+});
 
- export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/reducers/visible.js
+++ src/reducers/visible.js
@@ -1,35 +1,44 @@
-import { YELLOW_VISIBLE, RED_VISIBLE, GREEN_VISIBLE } from '../actions';
+import {
+  CHANGE_VISIBLE,
+  SET_CONTENT,
+  CHANGE_TEXT,
+  SET_HIGHLIGHT
+} from "../actions";
 
 const initialState = {
-  yellowVisible: false,
-  redVisible: false,
-  greenVisible: false,
+  text:
+    "Mussum Ipsum, cacilds vidis litro abertis. Sapien in monti palavris qui num significa nadis i pareci latim. Mais vale um bebadis conhecidiss, que um alcoolatra anonimis. Quem manda na minha terra sou euzis! Diuretics paradis num copo é motivis de denguis.",
+  visible: "red",
+  highlight: "yellow",
+  selections: {
+    red: [],
+    green: [],
+    yellow: []
+  }
 };
 
 export const visible = (state = initialState, action) => {
   switch (action.type) {
-    case YELLOW_VISIBLE:
+    case CHANGE_TEXT:
+      return { ...state, text: action.value };
+
+    case CHANGE_VISIBLE:
+      return { ...state, visible: action.value };
+
+    case SET_HIGHLIGHT:
+      return { ...state, highlight: action.value };
+
+    case SET_CONTENT:
+      const { color, value } = action;
       return {
         ...state,
-        yellowVisible: true,
-        redVisible: false,
-        greenVisible: false,
-      }
-    case RED_VISIBLE:
-      return {
-        ...state,
-        yellowVisible: false,
-        redVisible: true,
-        greenVisible: false,
-      }
-    case GREEN_VISIBLE:
-      return {
-        ...state,
-        yellowVisible: false,
-        redVisible: false,
-        greenVisible: true,
-      }    
+        selections: {
+          ...state.selections,
+          [color]: [...state.selections[color], value]
+        }
+      };
+
     default:
-      return state;  
+      return state;
   }
-}
+};
